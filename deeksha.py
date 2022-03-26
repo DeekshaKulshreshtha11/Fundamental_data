bfbcf5c4c21cbb49c960c14f031681cf8a9cfa1b
DeekshaKulshreshtha11 <deekshakulshreshtha11@gmail.com>
2022-03-26 10:35:16 +0530

    Modified

--- deeksha.py
+++ deeksha.py
@@ -1,5 +1,5 @@
 import smtplib
 server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-server.login("shreshthatechd@gmail.com", "Deetech#3")
-server.sendmail('shreshthatechd@gmial.com', 'deekshakulshrestha11@gmail.com', "Hello")
+server.login("emailId", "password")
+server.sendmail('From', 'To', "Message")
 server.quit()
